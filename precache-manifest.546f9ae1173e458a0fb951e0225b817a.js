b42d15ac76e737b86e69d933f5e6018f5b507979
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-02 15:25:55 +0200

    Updates

---
+++ precache-manifest.546f9ae1173e458a0fb951e0225b817a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbab4bb4ea45b5a517f3d7714826d2f0",
+    "url": "/restFull-api/index.html"
+  },
+  {
+    "revision": "e368267793498cd2c28f",
+    "url": "/restFull-api/static/css/main.4fa8c82f.chunk.css"
+  },
+  {
+    "revision": "ec670dd03932739141ef",
+    "url": "/restFull-api/static/js/2.fa6d69d8.chunk.js"
+  },
+  {
+    "revision": "e368267793498cd2c28f",
+    "url": "/restFull-api/static/js/main.c59a368b.chunk.js"
+  },
+  {
+    "revision": "07cf9b2dc84179ed939b",
+    "url": "/restFull-api/static/js/runtime-main.22cf0a25.js"
+  }
+]);
